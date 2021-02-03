1676451a4ee50e0ddb0a0a2cc82c56d49f3e041f
DanyilYatskov <68867876+DanyilYatskov@users.noreply.github.com>
2021-02-03 14:30:40 +0200

    added taskTwo.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './styles.css';
 import './js/taskOne';
+import './js/taskTwo';
---
+++ src/js/taskTwo.js
@@ -0,0 +1,39 @@
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback - функцию callback,
+//     а принимала всего два параметра allUsers и userName и возвращала промис.
+setTimeout(() => {
+  console.log('Task Two:');
+  const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+  ];
+
+  const toggleUserState = (allUsers, userName) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const usersCheck = Array.isArray(allUsers);
+        const nameCheck = users.find(user => user.name === userName)
+          ? true
+          : false;
+
+        if (usersCheck && nameCheck) {
+          const updatedUsers = allUsers.map(user =>
+            user.name === userName ? { ...user, active: !user.active } : user,
+          );
+          resolve(updatedUsers);
+        } else {
+          const e = 'Input error or user does not exist';
+          reject(e);
+        }
+      }, 1000);
+    });
+  };
+
+  const logger = updatedUsers => console.table(updatedUsers);
+  const errorHandler = e => console.log(e);
+
+  toggleUserState(users, 'Mango').then(logger).catch(errorHandler);
+  toggleUserState(users, 'Lux').then(logger).catch(errorHandler);
+  toggleUserState(users, 'asd').then(logger).catch(errorHandler);
+}, 2000);
